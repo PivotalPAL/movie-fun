d075a3f4ce1433c16e33d192c86a0ea02d677e79
PivotalPal <pal@engineerbetter.com>
2018-10-30 14:17:26 +0000

    Spring Bootification

---
+++ src/main/java/org/superbiz/moviefun/Application.java
@@ -0,0 +1,25 @@
+package org.superbiz.moviefun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import java.util.Map;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public ServletRegistrationBean RegistrationBean(ActionServlet myservlet){
+
+       ServletRegistrationBean myservletRegistrationBean = new ServletRegistrationBean();
+
+       return new ServletRegistrationBean(myservlet,"/moviefun/*");
+
+    }
+}
